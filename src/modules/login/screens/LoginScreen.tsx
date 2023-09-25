b3a294827ef781c28aebe134ff3f50224ff75a9b
Luka <luka.kato@tunts.com>
2023-09-25 11:30:14 -0300

    119. Mudança de estados dos componentes

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import Button from '../../../shared/buttons/button/button.tsx';
 import Input from '../../../shared/inputs/input/input.tsx';
 import {
@@ -10,6 +12,16 @@ import {
 } from '../styles/loginScreen.styles.ts';
 
 const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUserName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <ContainerLoginScreen>
       <BackgroundImage src="./background.png" />
@@ -19,8 +31,14 @@ const LoginScreen = () => {
           <TitleLogin level={2} type="secondary">
             Login
           </TitleLogin>
-          <Input title="Usuário" />
-          <Input title="Senha" />
+          <Input title="Usuário" margin="32px 0 0" onChange={handleUserName} value={username} />
+          <Input
+            title="Senha"
+            type="password"
+            margin="32px 0 0"
+            onChange={handlePassword}
+            value={password}
+          />
           <Button type="primary" margin="64px 0 16px 0">
             Entrar
           </Button>
